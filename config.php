ea2c990cf791a48c808a2f137573f4031031c83f
Roshan Shrestha <roshanshrestha@janakitech.com>
2013-06-12 14:48:50 +0545

    fixes in config.php

--- config.php
+++ config.php
@@ -4,11 +4,12 @@
 error_reporting(0);
 
 # Kannel provided default administration interface endpoint
-$server = "some-server-url:some-port";
+$server = "http://127.0.0.1:13000";
+
 # Admin password to access the administration commands in Kannel
 $admin_password = "some-admin-password";
+
 # Kannel status URL / File
-#$kannel_url = "status.xml";
-$kannel_url = "{$server}/status.xml?password=some-password";
+$kannel_url = "{$server}/status.xml?password=";
 
 ## ---------------- Configuration ends ----------------------------------------
