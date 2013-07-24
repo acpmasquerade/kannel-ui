5690afc9089efa0612f7cbeb878378cf3a4d2316
Dhruba Adhikari - acpmasquerade <dhrubaster@gmail.com>
2013-07-24 12:12:51 +0545

    config for push

--- config.php
+++ config.php
@@ -4,13 +4,13 @@
 error_reporting(0);
 
 # Kannel provided default administration interface endpoint
-$server = "http://ds1.sparrow:13024";
+$server = "http://kannel-server:30000";
 
 # Admin password to access the administration commands in Kannel
 $admin_password = "some-admin-password";
 
 # Kannel status URL / File
-$kannel_url = "{$server}/status.xml?password=st@tus_@_";
+$kannel_url = "{$server}/status.xml?password=status-password";
 
 ## ---------------- Configuration ends ----------------------------------------
 
